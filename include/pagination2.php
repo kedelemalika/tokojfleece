8c16d8751be991842ff155382d2ad98fb487a6e1
ruslanchasep <ruslan.chasep@gmail.com>
2015-02-15 04:01:11 +0700

    Update pagination2.php

--- include/pagination2.php
+++ include/pagination2.php
@@ -3,10 +3,10 @@
 	$batas = 4;
 	$page = isset($_GET['page']) ? $_GET['page'] : "";
 	$p = isset($_GET['p']) ? $_GET['p'] : 0;
-	if ($page=="guest" && $p!==0) {
+	if ($page=="guestbook" && $p!==0) {
 		$halam = $p - 1;
 		$mulai = $batas * $halam;
-	} else if ($page=="guest" and $p == 1) {
+	} else if ($page=="guestbook" and $p == 1) {
 		$mulai = 0;
 	} else {
 		$mulai = 0;
